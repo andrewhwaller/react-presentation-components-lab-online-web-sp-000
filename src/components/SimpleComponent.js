75b3bdfd4e799ae066d2e0a7d8ff3deba59fee8e
Andrew Waller <andrewhwaller@gmail.com>
2019-09-27 22:10:27 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,17 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react';
+
+class SimpleComponent extends Component {
+
+  state = {
+    mood: 'happy';
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.happy}
+      </div>
+    )
+  }
+}
